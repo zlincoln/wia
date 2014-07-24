ed8def36c6a66e785bfc3c46a2a7976636297509
Zachary Lincoln <zach@burlingtoncollective.com>
2014-07-24 18:10:30 -0400

    opacity transitions

--- presentation/js/main.js
+++ presentation/js/main.js
@@ -1,41 +1,46 @@
 var token = {
 	public: 'bqDwlbsTwtTegZcIlTlyz6XPnEYir2MvU3ezLZAqDzBuPHQyU3',
 	secret: 'hQ8V4cS9nrFf83eXCaPkJq1h5s59MaiNX3Ta643Bo2u5ykrtPu'
-}
-
-var callback = '';
-
-var request_data = {
+},
+request_data = {
 	url: 'http://api.tumblr.com/v2/blog/willmstevenson.com/posts/?offset=',
 	method: 'GET',
 	data: {
 		api_key: token.public,
 		limit: 10,
 		offset: 0,
-		jsonp: callback
+		jsonp: ''
 	}
-}
-
-var makeRequest = function(){
+},
+$target = $('#post-target'),
+makeRequest = function(){
 	$.ajax({
 		url: request_data.url,
 		method: request_data.method,
 		dataType: 'jsonp',
 		data: request_data.data,
 		success: function(data){
-			var $target = $('#post-target');
 			$.each(data.response.posts, function(index, post){
+				// var $node = $('<div class="post-template col-xs-4"><img class="img-responsive" src='+post.photos[0].alt_sizes[1].url+'></div>');
+				// $clone.find('img').attr('src', post.photos[0].alt_sizes[1].url);
+				// msnry.appended($node);
 				var $clone = $('#templates .post-template').clone(true);
 				$clone.find('img').attr('src', post.photos[0].alt_sizes[1].url);
 				$target.append($clone);
+				$target.masonry('addItems', $clone);
 				request_data.data.offset++;
 			});
 			imagesLoaded($target, function(){
-				msnry = new Masonry($target[0]);
+				$target.masonry();
 			});
 		}
 	});
 }
+
+$target.masonry();
+$target.masonry('on', 'layoutComplete', function(){
+	$('#post-target .post-template').addClass('loaded');
+});
 
 $(function(){
 	makeRequest();
