54f1b686b67f8119b8638ef8241c29ec58d3b22d
Zachary Lincoln <zach@burlingtoncollective.com>
2014-07-24 17:10:40 -0400

    images loaded + load more functionality

--- presentation/js/main.js
+++ presentation/js/main.js
@@ -3,23 +3,41 @@ var token = {
 	secret: 'hQ8V4cS9nrFf83eXCaPkJq1h5s59MaiNX3Ta643Bo2u5ykrtPu'
 }
 
+var callback = '';
+
 var request_data = {
-	url: 'http://api.tumblr.com/v2/blog/willmstevenson.com/posts/?api_key='+token.public+'&jsonp=',
-	method: 'GET'
+	url: 'http://api.tumblr.com/v2/blog/willmstevenson.com/posts/?offset=',
+	method: 'GET',
+	data: {
+		api_key: token.public,
+		limit: 10,
+		offset: 0,
+		jsonp: callback
+	}
 }
 
-$.ajax({
-	url: request_data.url,
-	method: request_data.method,
-	dataType: 'jsonp',
-	success: function(data){
-		console.log(data);
-		$.each(data.response.posts, function(index, post){
-			var clone = $('#templates .post-template').clone(true),
-					target = $('#post-target');
-			console.log(post);
-			clone.find('img').attr('src', post.photos[0].alt_sizes[1].url);
-			target.append(clone);
-		});
-	}
+var makeRequest = function(){
+	$.ajax({
+		url: request_data.url,
+		method: request_data.method,
+		dataType: 'jsonp',
+		data: request_data.data,
+		success: function(data){
+			var $target = $('#post-target');
+			$.each(data.response.posts, function(index, post){
+				var $clone = $('#templates .post-template').clone(true);
+				$clone.find('img').attr('src', post.photos[0].alt_sizes[1].url);
+				$target.append($clone);
+				request_data.data.offset++;
+			});
+			imagesLoaded($target, function(){
+				msnry = new Masonry($target[0]);
+			});
+		}
+	});
+}
+
+$(function(){
+	makeRequest();
+	$('#request-more').on('click', makeRequest);
 });
