218d057f7cf1ad9d2df6226fc19a7d293687379e
Zachary Lincoln <zach@burlingtoncollective.com>
2014-07-24 16:24:59 -0400

    tumblr requests working

--- presentation/js/main.js
+++ presentation/js/main.js
@@ -3,20 +3,23 @@ var token = {
 	secret: 'hQ8V4cS9nrFf83eXCaPkJq1h5s59MaiNX3Ta643Bo2u5ykrtPu'
 }
 
-var oauth = OAuth({
-	consumer: token,
-	signature_method: 'HMAC-SHA1'
-});
-
 var request_data = {
-	url: 'http://api.tumblr.com/v2/blog/willmstevenson.com/posts/',
+	url: 'http://api.tumblr.com/v2/blog/willmstevenson.com/posts/?api_key='+token.public+'&jsonp=',
 	method: 'GET'
 }
 
 $.ajax({
 	url: request_data.url,
 	method: request_data.method,
+	dataType: 'jsonp',
 	success: function(data){
-		console.log(data)
+		console.log(data);
+		$.each(data.response.posts, function(index, post){
+			var clone = $('#templates .post-template').clone(true),
+					target = $('#post-target');
+			console.log(post);
+			clone.find('img').attr('src', post.photos[0].alt_sizes[1].url);
+			target.append(clone);
+		});
 	}
 });
